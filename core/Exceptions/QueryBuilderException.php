ec426913bd93c17b12b01b8eec81b41f349b8527
Romulo Costa <Roco@MacBook-Pro-de-Edgar.local>
2025-08-04 14:37:24 -0300

    layout

--- core/Exceptions/QueryBuilderException.php
+++ core/Exceptions/QueryBuilderException.php
@@ -13,7 +13,7 @@ class QueryBuilderException extends \RuntimeException
      */
     protected $context = [];
 
-    public function __construct(string $message = "", int $code = 0, \Throwable $previous, array $context = [])
+    public function __construct(string $message = "", int $code = 0, ?\Throwable $previous = null, array $context = [])
     {
         parent::__construct($message, $code, $previous);
         $this->context = $context;
